f7b7e931cf06bee8e6dad597dfd24746cb98b68c
marick2003 <marick2003@users.noreply.github.com>
2018-04-16 19:16:58 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,108 @@
+var videos = [{
+    id: 1,
+    title: 'Behind the Illusions: Impossibly high-performance layout animations',
+    thumbnail: '//i.ytimg.com/vi/zVbLR7rI8ZU/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLAjiP06rSwpQF4HeF_5IEa1j9_k3A',
+    youtubeURL: '//www.youtube.com/embed/zVbLR7rI8ZU?list=PLZriQCloF6GApxBmkAcCp08VoI6mDFe2G',
+    speaker: 'David Khourshid',
+    likes: 0,
+    views: 0,
+    describe: "Have you ever wanted to dynamically animate layouts without sacrificing performance? In this talk, innovative illusions will be revealed that will transform your static user interfaces into lively, intuitive experiences for your users at 60 frames per second. We'll investigate even the most complex animated layouts and discover how certain techniques, such as FLIP, containment, clever transforms, pseudoelements, CSS variables, and more can make layout animations easier and smoother."
+  }, {
+    id: 2,
+    title: 'How to use CSS grid today in the real world',
+    thumbnail: '//i.ytimg.com/vi/fVX0xscGP9o/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBwfi3RYxD2fLrKubjZfVUGeKUwew',
+    youtubeURL: '//www.youtube.com/embed/fVX0xscGP9o?list=PLZriQCloF6GApxBmkAcCp08VoI6mDFe2G',
+    speaker: 'Brenda Storer',
+    likes: 0,
+    views: 0,
+    describe: "The new CSS Grid specification is here! Sure, it’s fun to play with, but is it truly ready or even practical to use for everyday work? As a designer and front end developer at a software development agency, I've been using CSS Grid in production websites since its initial release to browsers in March 2017 and I’m a huge fan. It is already making my life easier."
+  }, {
+    id: 3,
+    title: 'Faster fonts for speed fanatics',
+    thumbnail: '//i.ytimg.com/vi/iEinIwNppW4/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBOEfD3JJtfjTU6A2AwYt6c4AO47g',
+    youtubeURL: '//www.youtube.com/embed/iEinIwNppW4?list=PLZriQCloF6GApxBmkAcCp08VoI6mDFe2G',
+    speaker: 'Jeremy Wagner',
+    likes: 0,
+    views: 0,
+    describe: "Fonts! Guess those are a big thing now. You’re likely familiar with at least getting custom typefaces up and running. You probably even know some neat tricks beyond the basics, but do you know how to make your fonts fast? I mean, really fast? Fast is important. "
+  }, {
+    id: 4,
+    title: 'Can you see that OK? CSS tips for low-vision accessibility',
+    thumbnail: '//i.ytimg.com/vi/Wq2-cZ-vZBc/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBFyDrYMP3nLRdvvhdL508bkOUGOQ',
+    youtubeURL: '//www.youtube.com/embed/Wq2-cZ-vZBc?list=PLZriQCloF6GApxBmkAcCp08VoI6mDFe2G',
+    speaker: 'Julie Grundy',
+    likes: 0,
+    views: 0,
+    describe: "So much accessibility advice is based on the needs of screenreader users - but there are many more people with other types of vision problems who've never used a screenreader program in their life. Heck, at 9% of the Australian population, there are more people with vision impairments visiting your site than Internet Explorer users. What tools and techniques are they using, and how will that affect your design?"
+  }, {
+    id: 5,
+    title: 'Interaction of color systems',
+    thumbnail: '//i.ytimg.com/vi/AbpS3Z3xXE0/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBrAjL9WSWMBBC7enrsq8WvWhVP5g',
+    youtubeURL: '//www.youtube.com/embed/AbpS3Z3xXE0?list=PLZriQCloF6GApxBmkAcCp08VoI6mDFe2G',
+    speaker: 'Diana Mounter',
+    likes: 0,
+    views: 0,
+    describe: "Color is the most relative medium in art. This fact makes working with color in design systems particularly challenging. Few people seem to want to claim to be an expert in color—perhaps because there’s an immensely deep amount to learn, or perhaps it’s because color is one of those things we hate to get wrong. Whatever the reason, color remains a contentious but often critical part of design."
+  }, {
+    id: 6,
+    title: 'Magic tricks with Houdini',
+    thumbnail: '//i.ytimg.com/vi/FYWZrLpN5Po/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLDfBqTk48O0BeyK5J60ChhvNhemcA',
+    youtubeURL: '//www.youtube.com/embed/FYWZrLpN5Po?list=PLZriQCloF6GApxBmkAcCp08VoI6mDFe2G',
+    speaker: 'Sam Richard',
+    likes: 0,
+    views: 0,
+    describe: "This talk will focus on the working being done by the CSS Houdini Task Force to provide us with the ability to extend the browser’s render engines with JavaScript, above and beyond simply running JS on the main thread or a web worker. The power we've gotten to control the cache with Service Workers? That's but the first step of what Houdini will offer for render engines. "
+  }, {
+    id: 7,
+    title: 'Effective design and engineering collaboration',
+    thumbnail: '//i.ytimg.com/vi/REwp9rQQM7I/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLDRQXwbK0VOuDzXdz41y9T-Cfm2Ow',
+    youtubeURL: '//www.youtube.com/embed/REwp9rQQM7I?list=PLZriQCloF6GApxBmkAcCp08VoI6mDFe2G',
+    speaker: 'Theresa Ma',
+    likes: 0,
+    views: 0,
+    describe: "Hey designers, have you ever passed off a design to an engineer, only to later find out something unexpected? Maybe the design needs to be re-done because of technical tradeoffs? Maybe something different has been built? Maybe it’s lacking overall polish (why isn’t that button 3px to the left?)?"
+  }, {
+    id: 8,
+    title: 'Journeys: What makes a developer, really?',
+    thumbnail: '//i.ytimg.com/vi/LB-DvtCHOgE/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLD-Q6bmlxDT6aim_FvvbdANI62K7A',
+    youtubeURL: '//www.youtube.com/embed/LB-DvtCHOgE?list=PLZriQCloF6GApxBmkAcCp08VoI6mDFe2G',
+    speaker: 'Ivana McConnell',
+    likes: 0,
+    views: 0,
+    describe: "As part of Origins, I have been researching the different paths of coders and designers into the tech industry, and the paths are wonderfully varied and unique. However, we continue to raise up and legitimise certain paths over others, implicitly deciding who gets to be a developer of CSS or Javascript. That language then feeds into job postings and wider perceptions of what it is to write CSS, what it is to write Javascript (or even Javascript frameworks), and the legitimacy of different learning paths. From this, we create hierarchies and draw lines which result in different pay scales and fluctuating perceptions of the code we write. This threatens to devalue inclusivity work, and the development community as a whole— let’s learn about the unconventional paths into development and consciously pull away from the damaging hierarchies we create. Let’s celebrate these journeys instead!"
+  }];
+  window.onload = function () {
+
+
+    
+
+
+  };
+
+  window.addEventListener('load', function () {
+      
+    Vue.component('video-player', {
+        template: '#video-player',
+        props: ['videos','activeVideo'],
+        methods: {
+          chooseVideo: function (video) {
+            this.activeVideo = video
+            video.views += 1
+          },
+          addLike: function () {
+            this.activeVideo.likes += 1
+          }
+        }
+      })
+      
+      let app = new Vue({
+        el: '#app',
+        data () {
+          return {
+            videos,
+            activeVideo: videos[0]
+          }
+        }
+      })
+
+})
